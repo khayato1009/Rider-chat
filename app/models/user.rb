8951bd343ddb0c8095cf7673f8e59fa6bf22eca0
khayato1009 <hayato8810k@icloud.com>
2023-04-27 16:46:03 +0900

    バリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :bike_model, presence: true    
+  validates :motorcycle_model ,presence: true   
 has_many :user_rooms
 has_many :rooms, through: :user_rooms
 end
