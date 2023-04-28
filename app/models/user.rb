1a2853729eff92fc79f5f1889a9ea72c463a7b2b
khayato1009 <hayato8810k@icloud.com>
2023-04-28 16:30:21 +0900

    render修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :motorcycle_model 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
-  validates :motorcycle_model ,presence: true   
+  validates :motorcycle_model, presence: true  
+  
 has_many :user_rooms
 has_many :rooms, through: :user_rooms
 has_many :messages
